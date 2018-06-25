cedd682b2a3cb85022bf86e1cffcf94bb68f15d5
mateusgpontes <mateus.gpontes@hotmail.com>
2018-06-25 12:03:09 -0300

    Criar busca de sabores e ids

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const expressMongoDb = require('express-mongo-db');
 const bodyParser = require('body-parser');
+const ObjectID = require('mongodb').ObjectID;
 
 const app = express();
 
@@ -28,4 +29,37 @@ app.post('/churro', (req, res) =>{
     });
 });
 
+app.get('/churro/:id', (req, res) => {
+
+    let query = {
+        _id: ObjectID(req.params.id)
+    };
+
+
+    req.db.collection('sabores').findOne(query, (error, data) =>{
+        if(error){
+            res.status(500).send('Erro ao acessar o banco de dados');
+            return;
+        }
+
+        if(!data){
+            res.status(404).send();
+            return;
+        }
+            res.send(data);
+    });
+});
+
+app.get('/churros/:sabor', (req, res) => {
+    let query = {
+        sabor: req.params.sabor
+    }
+    req.db.collection('sabores').find(query).toArray((error, data) =>{
+        if(error){
+            res.status(500).send('Erro ao acessar o banco de dados');
+            return;
+        }
+            res.send(data);
+    });
+});
 app.listen(3000);
