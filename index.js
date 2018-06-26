56d07b96fcafe591e3071aca536a2c44fac46690
mateusgpontes <mateus.gpontes@hotmail.com>
2018-06-26 10:27:58 -0300

    Criar update e delete dos churros

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const expressMongoDb = require('express-mongo-db');
 const bodyParser = require('body-parser');
 const ObjectID = require('mongodb').ObjectID;
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(expressMongoDb('mongodb://localhost/mongo-churros'));
+app.use(cors());
 
 app.get('/churros', (req, res) => {
 
@@ -20,7 +22,15 @@ app.get('/churros', (req, res) => {
 });
 
 app.post('/churro', (req, res) =>{
-    req.db.collection('sabores').insert(req.body, (error) =>{
+
+    let churro = {
+        sabor: req.body.sabor,
+        recheio: req.body.recheio,
+        cobertura: req.body.cobertura
+    }
+
+    req.db.collection('sabores').insert(churro, (error) =>{
+
        if(error){
            res.status(500).send('Erro ao acessar o bando de dados');
            return;
@@ -34,7 +44,6 @@ app.get('/churro/:id', (req, res) => {
     let query = {
         _id: ObjectID(req.params.id)
     };
-
 
     req.db.collection('sabores').findOne(query, (error, data) =>{
         if(error){
@@ -62,4 +71,47 @@ app.get('/churros/:sabor', (req, res) => {
             res.send(data);
     });
 });
+
+app.put('/churro/:id', (req, res) => {
+
+    let query = {
+        _id: ObjectID(req.params.id)
+    };
+
+    let churro = {
+        sabor: req.body.sabor,
+        recheio: req.body.recheio,
+        cobertura: req.body.cobertura
+    }
+
+    req.db.collection('sabores').updateOne(query, churro, (error, data) =>{
+        if(error){
+            res.status(500).send('Erro ao acessar o banco de dados');
+            return;
+        }
+
+        if(!data){
+            res.status(404).send();
+            return;
+        }
+            res.send(data);
+    });
+});
+
+app.delete('/churro/:id', (req, res) => {
+
+    let query = {
+        _id: ObjectID(req.params.id)
+    };
+
+    req.db.collection('sabores').deleteOne(query, (error, data) =>{
+        if(error){
+            res.status(500).send('Erro ao acessar o banco de dados');
+            return;
+        }
+
+        res.send(data);
+    });
+});
+
 app.listen(3000);
